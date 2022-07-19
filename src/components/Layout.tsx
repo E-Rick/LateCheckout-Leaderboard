b309ba242072dec9f6c876c880f3f9c483c3f83c
Erick Martinez <erick@wrecs.studio>
2022-07-19 16:32:51 -0400

    add: Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+
+import Head from 'next/head';
+
+type Props = {
+	children?: ReactNode;
+	title?: string;
+};
+
+const Layout = ({ children, title = 'This is the default title' }: Props) => (
+	<>
+		<Head>
+			<title>{title}</title>
+			<meta charSet='utf-8' />
+			<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+			<link rel='shortcut icon' href='/images/favicon.ico' />
+		</Head>
+
+		{children}
+	</>
+);
+
+export default Layout;
