46954553cd21dfe5ca1fbe4282e31c8279f8bcda
Erick Martinez <erick@wrecs.studio>
2022-07-20 08:46:03 -0400

    add: user data

---
+++ src/data/users.ts
@@ -0,0 +1,98 @@
+export const users = [
+	{
+		name: 'Theo Tabah',
+		address: '0xC62A517b7Cf5126d8F709BFB3Dbcba8a4D8eD5ce',
+	},
+	{
+		name: 'Greg Isenberg',
+		address: '0x9F836913343C0B46771206De4018Fcdd0D76A271',
+	},
+	{
+		name: 'Theo Richardson',
+		address: '0x956C23b9E573FBBab69F2D4C06eb776c1bB98113',
+	},
+	{
+		name: 'Vyara Ndejuru',
+		address: '0xC21A69E9e83dE0960E889AddD2c7a32F518B8D12',
+	},
+	{
+		name: 'Lucas Uema',
+		address: '0xf536917a922861219AA9aDD8c3e95Fe88deE752d',
+	},
+	{
+		name: 'Madison Marlow',
+		address: '0xB02fCc2C6c4B83AEdadae901Bb1A06e655580bD5',
+	},
+	{
+		name: 'Adrian Pang',
+		address: '0xd906979081fcB040F00213E86fb887A12DE2c495',
+	},
+	{
+		name: 'Crystal Torres',
+		address: '0xA355f1EC00721955A696EC0AB135b6018030DA2a',
+	},
+	{
+		name: 'Dany Rizky',
+		address: '0x745296eb59376b154D3542D6bF7ee44AB2004cef',
+	},
+	{
+		name: 'Adi Bani',
+		address: '0x31057Cd3317638047b3Ff95E6Be240D9285708BC',
+	},
+	{
+		name: 'Arkan Faruza',
+		address: '0xBCE82Fc4943a3a8c40425f364F19b115C9737c54',
+	},
+	{
+		name: 'Deddy Irawan',
+		address: '0x8a1b8f2Ceb7239C71c6Ec56F866023833B48d45e',
+	},
+	{
+		name: 'Fikrun Nashih',
+		address: '0x428E9a4465131D5Ee9226878Ff50e80c3dEed868',
+	},
+	{
+		name: 'Rizqi Nizamil',
+		address: '0x5f435f6378FAeb6d079cA9FB0A8BEAFFF082B38e',
+	},
+	{
+		name: 'Adrien Devleschoudere',
+		address: '0xa8704208FB8f505D0ad8476D99Dd584C93C11729',
+	},
+	{
+		name: 'Ilham Farobi',
+		address: '0xf9585c2CD430d7E0985a08548d7090C52e21d223',
+	},
+	{
+		name: 'Julian Anderson',
+		address: '0xca2133BC59F1cD923E4840564a7f19046530f15b',
+	},
+	{
+		name: 'Gabriel Arnone',
+		address: '0x01E2dAb248718a724546ABdCD2d27bFb17fa0b38',
+	},
+	{
+		name: 'Bayo Santosa',
+		address: '0x0C716400380DDEe8901d283Dd6C3D47D6a72dae4',
+	},
+	{
+		name: 'MacBrennan Peet',
+		address: '0x1dCA692b9EE454e4FcF530294F81497f6182e2c2',
+	},
+	{
+		name: 'Kevin Chaiken',
+		address: '0x23788dB283f624EA63A33929fF5612EED257c7A3',
+	},
+	{
+		name: 'Sakib Rasul',
+		address: '0xF3A66cb806d91a648204ED2a9293Ff38c75C1339',
+	},
+	{
+		name: 'Alex Nemeroff',
+		address: '0xbD109Fd9aE86FA7b4e8d97336e4754d9dcE0f098',
+	},
+	{
+		name: 'Erick Martinez Jr.',
+		address: '0x392e999325468bf113fdcB378581f6bdFE4C53F2',
+	},
+]
